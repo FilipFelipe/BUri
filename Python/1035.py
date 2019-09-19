28e14ed7e4a0c7e69cae3ae9c2ea6717d28c4c8f
Filip Felipe <filip_junio@hotmail.com>
2019-09-19 04:35:23 -0300

    Add 1035.py

---
+++ Python/1035.py
@@ -0,0 +1,10 @@
+resp = input().split()
+a = int(resp[0])
+b = int(resp[1])
+c = int(resp[2])
+d = int(resp[3])
+
+if b > c and d > a and (c+d) > (a+b) and c >= 0 and d >= 0 and (a%2) == 0:
+    print("Valores aceitos")
+else:
+    print("Valores nao aceitos")
